6690077de7958008bd1c5ac9dadfb03ffab6bee3
Roman <ramzesnic@gmail.com>
2018-12-25 23:00:49 +0300

    add eslint and codeclimate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import readlineSync from 'readline-sync'; 
+import readlineSync from 'readline-sync';
+
 export default () => {
   const answer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${answer}!`);
