c82c6820755d91a398e08a791489f40baf5bc672
ramzesnic <46097973+ramzesnic@users.noreply.github.com>
2018-12-25 17:58:38 +0300

    fix indent

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-// не коммитит когда делаю только reindent
 export default () => {
 	const answer = readlineSync.question('May I have your name? ');
 	console.log(`Hello, ${answer}!`);
