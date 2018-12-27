32dd7eaae97db3d76e5809888bf69f0ff7cf82ea
Roman <ramzesnic@gmail.com>
2018-12-27 22:58:15 +0300

    fix gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,18 +2,18 @@ import engine, { getRandom } from '..';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const nod = (a, b) => {
+  if (a === b) return a;
+  if (a > b) {
+    return nod(a - b, b);
+  }
+  return nod(a, b - a);
+};
+
 const game = () => {
   const num1 = getRandom(1, 100);
   const num2 = getRandom(1, 100);
-
-  const iter = (a, b) => {
-    if (a === b) return a;
-    if (a > b) {
-      return iter(a - b, b);
-    }
-    return iter(a, b - a);
-  };
-  const trueAnswer = iter(num1, num2);
+  const trueAnswer = nod(num1, num2);
   const question = `${num1} ${num2}`;
   return {
     question,
