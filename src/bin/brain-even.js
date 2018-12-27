bfe4fafe085f4bbf040f9d0f16706d6fe734cd61
Roman <ramzesnic@gmail.com>
2018-12-27 09:49:04 +0300

    fix structure project errors

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import getName, { isEven } from '..';
+import { brainEven } from '..';
 
-console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
-const name = getName();
-console.log(`Hello, ${name}!\n`);
-const min = 1;
-const max = 100;
-const count = 3;
-console.log(isEven(count, min, max, name));
+brainEven();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import readlineSync from 'readline-sync';
 
-export default () => readlineSync.question('May I have your name? ');
+const getName = () => readlineSync.question('May I have your name? ');
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const isEven = (count, min, max, name) => {
-  if (count === 0) return `Congratulations, ${name}!`;
-  const randomNum = getRandom(min, max);
-  const trueAnswer = randomNum % 2 ? 'no' : 'yes';
-  const answer = readlineSync.question(`Question: ${randomNum}\nYour answer: `);
-  if (answer !== trueAnswer) {
-    const message = `'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}.\n Let's try again, ${name}!'`;
-    return message;
-  }
-  console.log('Correct!');
-  return isEven(count - 1, min, max, name);
+const isEven = question => question % 2 === 0;
+
+export const brainEven = () => {
+  const countRaund = 3;
+  const min = 1;
+  const max = 100;
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  const name = getName();
+  console.log(`Hello, ${name}!\n`);
+
+  const iter = (count) => {
+    if (count === 0) return `Congratulations, ${name}!`;
+    const question = getRandom(min, max);
+    const trueAnswer = isEven(question) ? 'yes' : 'no';
+    const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
+    if (answer !== trueAnswer) {
+      const message = `'${answer}' is wrong answer ;(. Correct answer was '${trueAnswer}.\n Let's try again, ${name}!'`;
+      return message;
+    }
+    console.log('Correct!');
+    return iter(count - 1);
+  };
+  return console.log(iter(countRaund));
 };
+
+export default getName;
