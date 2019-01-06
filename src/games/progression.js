d2571680b2730402ac06acdae494c7e1c36b610e
Roman <ramzesnic@gmail.com>
2019-01-06 19:05:57 +0300

    mutable to imutable

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,20 +4,14 @@ import getRandom from '../utils';
 const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const progressionGenerator = (start, step, length) => {
-  const progression = [];
-  for (let i = 0; i < length; i += 1) {
-    progression.push(start + step * i);
-  }
-  return progression;
-};
-
 const game = () => {
   const start = getRandom(0, 100);
   const step = getRandom(1, 5);
   const hiddenElementPosition = getRandom(0, progressionLength);
-  const progression = progressionGenerator(start, step, progressionLength);
-  const trueAnswer = progression[hiddenElementPosition];
+  const progression = Array(...new Array(progressionLength)).map(
+    (value, index) => start + step * index,
+  );
+  const trueAnswer = progression[hiddenElementPosition].toString();
   const question = progression.map((value, index) => (index === hiddenElementPosition ? '..' : value)).join(' ');
   return {
     question,
